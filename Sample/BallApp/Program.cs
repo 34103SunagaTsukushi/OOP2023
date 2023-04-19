cf6b862a0a2e8c8b0a82414850651dce1b476918
須永　築 <OIBC34103@ojs.ac.jp>
2023-04-19 11:55:58 +0900

    複数のボール出力対応

--- Sample/BallApp/Program.cs
+++ Sample/BallApp/Program.cs
@@ -13,6 +13,9 @@ namespace BallApp {
         private SoccerBall soccerBall;
         private PictureBox pb;
 
+        private List<SoccerBall> balls = new List<SoccerBall>(); //ボールインスタンス格納用
+        private List<PictureBox> pbs = new List<PictureBox>(); //表示用
+
         static void Main(string[] args) {
             Application.Run(new Program());
         }
@@ -27,6 +30,8 @@ namespace BallApp {
             this.Text = "BallGame";
 
             this.MouseClick += Program_MouseClick;
+
+            
 
 
 
@@ -48,14 +53,22 @@ namespace BallApp {
             pb.Size = new Size(50, 50); //画像の表示サイズ
             pb.SizeMode = PictureBoxSizeMode.StretchImage; //画像の表示モード
             pb.Parent = this;
+            
+            balls.Add(soccerBall);
+            pbs.Add(pb);
 
             moveTimer.Start(); //タイマースタート
         }
         //タイマータイムアウト時のイベントハンドラ
         private void MoveTimer_Tick(object sender, EventArgs e) {
+
+            for (int i = 0; i < balls.Count; i++)
+            {
+                balls[i].Move(); //移動
+                pbs[i].Location =new Point((int)balls[i].PosX, (int)balls[i].PosY); //画像の位置
+            }
             
-            soccerBall.Move(); //移動
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY); //画像の位置
+            
         }
     }
 }
