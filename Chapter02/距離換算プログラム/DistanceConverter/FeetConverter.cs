6d492c6414919329b52dec1e6ceb9541b9747eda
須永　築 <OIBC34103@ojs.ac.jp>
2023-05-10 11:25:11 +0900

    距離換算プログラムの完成

--- Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
@@ -5,18 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DistanceConverter {
-
+  
     //フィートとメートルの単位変換クラス
     public static class FeetConverter {
+        //定数
+        private const double ratio = 0.3048;
+        //public static readonly double ratio = 0.3048;
 
         //フィートからメートルを求める
         public static double ToMeter(double feet) {
-            return feet * 0.3048;
+            return feet * ratio;
         }
 
         //メートルからフィートを求める
         public static double FromMeter(double meter) {
-            return meter / 0.3048;
+            return meter / ratio;
         }
     }
 }
