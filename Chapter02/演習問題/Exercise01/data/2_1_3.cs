06444e0ec7466311addb2c596f3e019574fce22a
須永　築 <OIBC34103@ojs.ac.jp>
2023-05-17 11:21:36 +0900

    P79問題2_1完成

--- Chapter02/演習問題/Exercise01/data/2_1_3.cs
+++ Chapter02/演習問題/Exercise01/data/2_1_3.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exercise01 {
-    class Program {
-        static void Main(string[] args) {
-            //2_1_3
-            var songs = new Song[] {
+            //2.1.3
+            /*var songs = new Song[] {
                 new Song("Let it be", "The Beatles", 243),
                 new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
                 new Song("Close To You", "Carpenters", 276),
@@ -24,17 +15,4 @@ namespace Exercise01 {
             lists.Add(new Song("Honesty", "Billy Joel", 231));
             lists.Add(new Song("I Will Always Love You", "Whitney Houston", 273));
 
-            PrintSongs(lists);
-
-
-        }
-
-        private static void PrintSongs(IEnumerable<Song> songs) {
-            foreach(var song in songs) {
-                Console.WriteLine("{0},{1},{2:m\\:ss}", song.Title, song.ArtistName,TimeSpan.FromSeconds( song.Length));
-                Console.WriteLine();
-
-            }
-        }
-    }
-}
+            PrintSongs(lists);*/
