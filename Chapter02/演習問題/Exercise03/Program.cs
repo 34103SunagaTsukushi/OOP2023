a6b7352eed3f383650d2063ac2a578652624345e
須永　築 <OIBC34103@ojs.ac.jp>
2023-05-22 10:16:14 +0900

    演習問題2.3　I/F変更

--- Chapter02/演習問題/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Program.cs
@@ -7,6 +7,25 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
+            var sales = new SalesCounter(@"data\sales.csv");//..\ 一階層上
+            Console.WriteLine("**売上集計**");
+            Console.WriteLine("1.店舗別売上");
+            Console.WriteLine("2.商品カテゴリー別売上");
+
+            int ans = int.Parse(Console.ReadLine());
+
+            if (ans == 1) {
+                var amountPerStore = sales.GetPerStoreSales();
+                foreach (var obj in amountPerStore) {
+                    Console.WriteLine("{0}{1:C}", obj.Key, obj.Value);
+                }
+            }
+            else if(ans == 2) {
+                var amountPerCategory = sales.GetPerCategorySales();
+                foreach (var obj in amountPerCategory) {
+                    Console.WriteLine("{0}{1:C}", obj.Key, obj.Value);
+                }
+            }
         }
     }
 }
