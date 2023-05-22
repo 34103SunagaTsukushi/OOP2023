81d387e99ef4853876664cbcbefc0a1bd866f8ad
須永　築 <OIBC34103@ojs.ac.jp>
2023-05-22 09:35:10 +0900

    P79 問題2.3新規作成

---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            if(args.Length < 3) {
+                Console.WriteLine("数値が入力されていません。") ;
+            }
+            else if (args.Length >= 3 && args[0] == "-tom") {
+                PrintInchToMeterList(int.Parse(args[1]), int.Parse(args[2]));
+            }
+            else if (args.Length >= 3 && args[0] == "-toi") {
+                PrintMeterToInchList(int.Parse(args[1]), int.Parse(args[2]));
+            }
+        }
+
+        private static void PrintInchToMeterList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0} inch = {1:0.0000} m", inch, meter);
+            }
+        }
+
+        private static void PrintMeterToInchList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double inch = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} inch", meter, inch);
+            }
+        }
+    }
+}
