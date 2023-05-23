a7c2b69cf01257cf4a2655efb31766063cc1ca8a
須永　築 <OIBC34103@ojs.ac.jp>
2023-05-23 10:30:51 +0900

    P87まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,19 +8,17 @@ namespace Section01 {
     class Program {
 
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            int count = Count(numbers, delegate (int n) { return n % 2 == 0; });
-            Console.WriteLine(count);
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, 15, 17, 20, 30, 25, 23, 31, 35, };
 
-        }
-        static public int Count(int [] numbers,Predicate<int> judge) {
-            int count = 0;
-            foreach(var n in numbers){
-                if (judge(n) == true) {
-                    count++;
-                }
-            }
-            return count;
+            //5の倍数をカウントする
+            //int count = numbers.Count( n =>  n > 0 && n % 5 == 0);
+
+            //合計値
+            var sum = numbers.Where(n => n % 2 == 0).Average();
+
+            
+            Console.WriteLine(sum);
+
         }
     }
 }
