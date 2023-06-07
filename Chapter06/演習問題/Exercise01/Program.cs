1ae2febc5fc8b1eb88c4ddd96dbff7733e6c98e4
須永　築 <OIBC34103@ojs.ac.jp>
2023-06-07 11:39:21 +0900

    6.1.3まで

---
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_4(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_5(numbers);
+
+        }
+
+        private static void Exercise1_1(int[] numbers) {
+            Console.WriteLine(numbers.Max());
+            
+        }
+
+        private static void Exercise1_2(int[] numbers) {
+            var lastnumbers = numbers.Skip(numbers.Length-2);
+            foreach(var number in lastnumbers) {
+                Console.Write(number+",");
+            }
+            Console.WriteLine();
+        }
+
+        private static void Exercise1_3(int[] numbers) {
+            var strNumbers = numbers.Select(x => x.ToString());
+            foreach(var number in strNumbers) {
+                Console.Write(number + ",");
+            }
+            Console.WriteLine();
+        }
+
+        private static void Exercise1_4(int[] numbers) {
+            var numbersAsc = numbers.OrderBy(x => x);
+            foreach(var number in numbersAsc) {
+                Console.WriteLine(number);
+            }
+        }
+
+        private static void Exercise1_5(int[] numbers) {
+            var ans = numbers.Where(x => x >= 10).Distinct().Count();
+            Console.WriteLine(ans);
+            
+            
+        }
+    }
+}
