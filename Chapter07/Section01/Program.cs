cd0a8a270bf653f374322d9e2806ea20f4628078
須永　築 <OIBC34103@ojs.ac.jp>
2023-06-23 11:49:11 +0900

    Dictionaryを使った県庁所在地の登録プログラム

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -61,15 +61,15 @@ namespace Section01 {
             Console.WriteLine("一覧表示:1,県名指定:2");
             int a = int.Parse(Console.ReadLine());
             if(a == 1) {//一覧表示
-                foreach(var item in prefDict) {
-                    Console.WriteLine("{0}({1},{2})",item.Key,item.Value.City,item.Value.Population);
+                foreach(var item in prefDict.OrderByDescending(x => x.Value.Population)) {
+                    Console.WriteLine("{0}【{1}：{2}】",item.Key,item.Value.City,item.Value.Population);
                 }
             }
             else {//県名指定表示
                 Console.Write("県名を入力:");
                 string ans = Console.ReadLine();
                 if (prefDict.ContainsKey(ans)) {
-                    Console.WriteLine("{0}({1})です。", prefDict[ans].City,prefDict[ans].Population);
+                    Console.WriteLine("【{0}：{1}】)です。", prefDict[ans].City,prefDict[ans].Population);
                 }
                 else {
                     Console.WriteLine("入力した県名が存在していません。");
