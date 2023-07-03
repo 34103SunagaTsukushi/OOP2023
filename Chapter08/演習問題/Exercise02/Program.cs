1697954defd573f03203fdc7887f0700fb6523fd
須永　築 <OIBC34103@ojs.ac.jp>
2023-07-03 13:34:49 +0900

    8.2まで

--- Chapter08/演習問題/Exercise02/Program.cs
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -8,7 +8,26 @@ namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
 
+            foreach(var dayofweek in Enum.GetValues(typeof(DayOfWeek))) {
 
+                DateTime nextweek = NextWeek(DateTime.Today, (DayOfWeek)dayofweek);
+
+               Console.WriteLine("{0}の次週の{1}:{2}",DateTime.Today.ToString("yyyy/MM/dd"),
+                   nextweek.ToString("dddd"),nextweek.ToString("yyyy/MM/dd (ddd)"));
+
+            }
+
+
+            //Console.WriteLine( NextWeek(DateTime.Today, DayOfWeek.Sunday));
+
+
+        }
+        public static DateTime NextWeek(DateTime date,DayOfWeek dayOfWeek) {
+            var days = (int)dayOfWeek - (int)(date.DayOfWeek);
+            if(days <= 0)
+                days += 7;
+
+            return date.AddDays(days);
         }
     }
 }
