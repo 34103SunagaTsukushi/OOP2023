201028132a4273fc3fa210120113f44eef06ef9e
須永　築 <OIBC34103@ojs.ac.jp>
2023-10-11 12:10:07 +0900

    Chapter15 Exercise1.2途中

---
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+
+
+        }
+
+        private static void Exercise1_2() {
+            var books = Library.Books.Where(b => b.Price == Library.Books.Max(c => c.Price));
+            foreach(var book in books) {
+                Console.WriteLine("タイトル:{0}:{1}円({2}年)",book.Title,book.Price,book.PublishedYear);
+            }
+            
+        }
+
+        private static void Exercise1_3() {
+            
+        }
+
+        private static void Exercise1_4() {
+            
+        }
+
+        private static void Exercise1_5() {
+            
+        }
+
+        private static void Exercise1_6() {
+            
+        }
+
+        private static void Exercise1_7() {
+            
+        }
+
+        private static void Exercise1_8() {
+            
+        }
+    }
+}
